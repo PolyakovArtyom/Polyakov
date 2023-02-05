e503423e2c556e109e84ce479be5a8d933889a28
PolyakovArtyom <cool.artback2013@gmail.com>
2023-02-05 13:25:35 +0300

    Made displaying of countries and genres lists

--- app/src/main/java/com/example/polyakov/SingleFilmFragment.kt
+++ app/src/main/java/com/example/polyakov/SingleFilmFragment.kt
@@ -27,33 +27,46 @@ class SingleFilmFragment(private val film: Film) : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val dataSource = CloudDataSource()
         val error = view.findViewById<View>(R.id.singleFilmError)
         val errorBtn = view.findViewById<Button>(R.id.retryBtn)
         errorBtn.setOnClickListener {
             view.findViewById<View>(R.id.card_root).visibility = View.VISIBLE
             error.visibility = View.VISIBLE
+            dataSource.getFilm(film.filmID)
         }
 
-        val dataSource = CloudDataSource()
+        val imageView = view.findViewById<ImageView>(R.id.cardImage)
+        val filmName = view.findViewById<TextView>(R.id.filmName)
+        val description = view.findViewById<TextView>(R.id.filmDescription)
+        val genres = view.findViewById<TextView>(R.id.filmGenres)
+        val countries = view.findViewById<TextView>(R.id.filmCountries)
+        val year = view.findViewById<TextView>(R.id.filmYear)
+
         dataSource.callbackSuccessFilm = { fullFilm ->
-            val imageView = view.findViewById<ImageView>(R.id.cardImage)
-            Glide.with(requireContext()).load(film.posterURL).into(imageView)
-            val filmName = view.findViewById<TextView>(R.id.filmName)
-            filmName.text = film.nameRu
-            val description = view.findViewById<TextView>(R.id.filmDescription)
+            Glide.with(requireContext()).load(fullFilm.posterURL).into(imageView)
+            filmName.text = fullFilm.nameRu
             description.text = fullFilm.description
-            val genres = view.findViewById<TextView>(R.id.filmGenres)
-            genres.text = film.genres[0].genre
-            val countries = view.findViewById<TextView>(R.id.filmCountries)
-            countries.text = film.countries[0].country
-            val year = view.findViewById<TextView>(R.id.filmYear)
-            year.text = film.year
+            var genresText = ""
+            for (i in 0 until fullFilm.genres.size) {
+                genresText += if (i == fullFilm.genres.size - 1) film.genres[i].genre
+                else "${fullFilm.genres[i].genre}, "
+            }
+            genres.text = fullFilm.genres[0].genre
+            var countriesText = ""
+            for (i in 0 until fullFilm.countries.size) {
+                countriesText += if (i == fullFilm.countries.size - 1) film.countries[i].country
+                else "${fullFilm.countries[i].country}, "
+            }
+            countries.text = countriesText
+            year.text = fullFilm.year.toString()
         }
+
         dataSource.callbackError = {
             view.findViewById<View>(R.id.card_root).visibility = View.INVISIBLE
             error.isVisible = true
-            dataSource.getFilm(film.filmID)
         }
+
         dataSource.getFilm(film.filmID)
     }
 }
